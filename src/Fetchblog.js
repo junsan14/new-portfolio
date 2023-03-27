d3c8ce5653c247906af1ba7de67a67aa7d9dc7cb
junsan14 <jusnanjunsan14@gmail.com>
2023-03-27 12:11:20 +0900

    0327_1

--- src/Fetchblog.js
+++ src/Fetchblog.js
@@ -20,11 +20,16 @@ function FetchAllPost() {
       return(
         <>
           {data.map((item,i) => {
-          let date = new Date(item.date).getFullYear()+ "/" 
-          +new Date(item.date).getMonth().toString().padStart(2, '0')+ "/" 
-          +new Date(item.date).getDate().toString().padStart(2, '0')+ "　"
-          +new Date(item.date).getHours().toString().padStart(2, '0')+ ":"
-          +new Date(item.date).getMinutes().toString().padStart(2, '0');   
+          let publishDate = new Date(item.date).getFullYear()+ "/" 
+            +(new Date(item.date).getMonth()+1).toString().padStart(2, '0')+ "/" 
+            +new Date(item.date).getDate().toString().padStart(2, '0')+ " "
+            +new Date(item.date).getHours().toString().padStart(2, '0')+ ":"
+            +new Date(item.date).getMinutes().toString().padStart(2, '0'); 
+          let upadtedate = new Date(item.modified).getFullYear()+ "/" 
+            +(new Date(item.modified).getMonth()+1).toString().padStart(2, '0')+ "/" 
+            +new Date(item.modified).getDate().toString().padStart(2, '0')+ " "
+            +new Date(item.modified).getHours().toString().padStart(2, '0')+ ":"
+            +new Date(item.modified).getMinutes().toString().padStart(2, '0');   
           let thumbnail = item["_embedded"]["wp:featuredmedia"]?item["_embedded"]["wp:featuredmedia"][0]["source_url"]:noImg;
 
             return(
@@ -37,7 +42,8 @@ function FetchAllPost() {
                         {item.excerpt.rendered.replace(/(<([^>]+)>)/gi, '')}
                     </p>
                     <p className="article_remarks_date">
-                        公開日:　{date}<br/>
+                        更新日:　{upadtedate}<br/>
+                        公開日:　{publishDate}<br/>
                     </p>
 
                   </div>
@@ -117,13 +123,13 @@ function FetchPageData(){
       //console.log(ele)
      $(ele).css("position", "relative")
      $(ele).wrap(
-      `<div class="markup-area">
+      `<div class="markup-area" id=${i}>
       </div>
       `
      );
      $(ele).append(
       `
-      <div class="markup-area-copy">
+      <div class="markup-area-copy" id=${i}>
        <p>copy</p>
       </div>
       `
@@ -131,11 +137,11 @@ function FetchPageData(){
       
      });
      $(".markup-area-copy").on("click", function(){
-      let that = $(this)
       
-      let copiedText =that.children("code").text();
-      console.log($(this).children("code"))
-      that.children(".markup-area-copy").text("copied")
+      let copiedText = $(this).prev().text();
+      $(this).children("p").text("copied")
+      console.log($(this).prev().text())
+      //that.children(".markup-area-copy").text("copied")
       return navigator.clipboard.writeText(copiedText);
     })
   })
@@ -152,21 +158,27 @@ function FetchPageData(){
 
   const Render = ()=>{
     if(data){
-      let date = new Date(data.date).getFullYear()+ "/" 
-          +new Date(data.date).getMonth().toString().padStart(2, '0')+ "/" 
+      let publishDate = new Date(data.date).getFullYear()+ "/" 
+          +(new Date(data.date).getMonth()+1).toString().padStart(2, '0')+ "/" 
           +new Date(data.date).getDate().toString().padStart(2, '0')+ " "
           +new Date(data.date).getHours().toString().padStart(2, '0')+ ":"
           +new Date(data.date).getMinutes().toString().padStart(2, '0'); 
+      let upadtedate = new Date(data.modified).getFullYear()+ "/" 
+          +(new Date(data.modified).getMonth()+1).toString().padStart(2, '0')+ "/" 
+          +new Date(data.modified).getDate().toString().padStart(2, '0')+ " "
+          +new Date(data.modified).getHours().toString().padStart(2, '0')+ ":"
+          +new Date(data.modified).getMinutes().toString().padStart(2, '0'); 
+        
       return(
         <>
               <h1 className="section_content_title">{data.title.rendered}</h1>
               <div className="article" id={data.id} key={data.id}>
                 <div className="article_date">
                     <p className="article_date_publish">
-                        更新日: {date} 
+                        更新日: {upadtedate} 
                     </p>
                     <p className="article_date_publish">
-                        公開日: {date}
+                        公開日: {publishDate}
                     </p>
                 </div>
                 <div className="article_content">    
@@ -190,7 +202,5 @@ function FetchPageData(){
    //console.log((data.content))
 
 }
-
-
 
 export {FetchAllPost,FetchThreePost,FetchPageData};
